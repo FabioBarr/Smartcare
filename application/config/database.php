3add1747cbf1406ceea2fa5124671c0cda4652de
Martelotte <leonardomartelotte@gmail.com>
2019-08-25 19:48:21 -0300

    Conexão persistente com BD

--- application/config/database.php
+++ application/config/database.php
@@ -12,7 +12,7 @@ $db['default'] = array(
 	'database' => 'u819903730_inovi',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
-	'pconnect' => FALSE,
+	'pconnect' => TRUE,
 	'db_debug' => (ENVIRONMENT !== 'production'),
 	'cache_on' => FALSE,
 	'cachedir' => '',
