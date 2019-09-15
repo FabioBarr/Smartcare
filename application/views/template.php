b1fe26dd608c6261f3fccb7771e9712e907f7588
GABRIELROLAND <32078927+GABRIELROLAND@users.noreply.github.com>
2019-09-14 22:23:13 -0300

    Função Cadastrar sensor adicionada. Template mantido.
    Co-autor= Guilherme alexandre e Bruno Legentil.

--- application/views/template.php
+++ application/views/template.php
@@ -20,7 +20,7 @@
 <!-- ATUALIZA Sensores -->
 <script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
 
-<script>
+<!--<script>
 function leitura() {
 
   $.ajax({
@@ -58,9 +58,9 @@ function leitura() {
         }
   });
 }
-interval = setInterval(leitura, 5000); 
+interval = setInterval(leitura, 5000);
 
-</script>
+</script>-->
 </head>
 
 <body id="page-top">
@@ -186,160 +186,7 @@ interval = setInterval(leitura, 5000);
       <div id="content">
 
         <!-- Topbar -->
-        <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
 
-          <!-- Sidebar Toggle (Topbar) -->
-          <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
-            <i class="fa fa-bars"></i>
-          </button>
-
-          <!-- Topbar Search -->
-          <form class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
-            <div class="input-group">
-              <input type="text" class="form-control bg-light border-0 small" placeholder="Buscar..." aria-label="Search" aria-describedby="basic-addon2">
-              <div class="input-group-append">
-                <button class="btn btn-primary" type="button">
-                  <i class="fas fa-search fa-sm"></i>
-                </button>
-              </div>
-            </div>
-          </form>
-
-          <!-- Topbar Navbar -->
-          <ul class="navbar-nav ml-auto">
-
-            <!-- Nav Item - Search Dropdown (Visible Only XS) -->
-            <li class="nav-item dropdown no-arrow d-sm-none">
-              <a class="nav-link dropdown-toggle" href="#" id="searchDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <i class="fas fa-search fa-fw"></i>
-              </a>
-              <!-- Dropdown - Messages -->
-              <div class="dropdown-menu dropdown-menu-right p-3 shadow animated--grow-in" aria-labelledby="searchDropdown">
-                <form class="form-inline mr-auto w-100 navbar-search">
-                  <div class="input-group">
-                    <input type="text" class="form-control bg-light border-0 small" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2">
-                    <div class="input-group-append">
-                      <button class="btn btn-primary" type="button">
-                        <i class="fas fa-search fa-sm"></i>
-                      </button>
-                    </div>
-                  </div>
-                </form>
-              </div>
-            </li>
-
-            <!-- Nav Item - Alerts -->
-            <li class="nav-item dropdown no-arrow mx-1">
-              <a class="nav-link dropdown-toggle" href="#" id="alertsDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <i class="fas fa-bell fa-fw"></i>
-                <!-- Counter - Alerts -->
-                <span class="badge badge-danger badge-counter">3+</span>
-              </a>
-              <!-- Dropdown - Alerts -->
-              <div class="dropdown-list dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="alertsDropdown">
-                <h6 class="dropdown-header">
-                  Alerts Center
-                </h6>
-                <a class="dropdown-item d-flex align-items-center" href="#">
-                  <div class="mr-3">
-                    <div class="icon-circle bg-primary">
-                      <i class="fas fa-file-alt text-white"></i>
-                    </div>
-                  </div>
-                  <div>
-                    <div class="small text-gray-500">December 12, 2019</div>
-                    <span class="font-weight-bold">A new monthly report is ready to download!</span>
-                  </div>
-                </a>
-                <a class="dropdown-item d-flex align-items-center" href="#">
-                  <div class="mr-3">
-                    <div class="icon-circle bg-success">
-                      <i class="fas fa-donate text-white"></i>
-                    </div>
-                  </div>
-                  <div>
-                    <div class="small text-gray-500">December 7, 2019</div>
-                    $290.29 has been deposited into your account!
-                  </div>
-                </a>
-                <a class="dropdown-item d-flex align-items-center" href="#">
-                  <div class="mr-3">
-                    <div class="icon-circle bg-warning">
-                      <i class="fas fa-exclamation-triangle text-white"></i>
-                    </div>
-                  </div>
-                  <div>
-                    <div class="small text-gray-500">December 2, 2019</div>
-                    Spending Alert: We've noticed unusually high spending for your account.
-                  </div>
-                </a>
-                <a class="dropdown-item text-center small text-gray-500" href="#">Show All Alerts</a>
-              </div>
-            </li>
-
-            <!-- Nav Item - Messages -->
-            <li class="nav-item dropdown no-arrow mx-1">
-              <a class="nav-link dropdown-toggle" href="#" id="messagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <i class="fas fa-envelope fa-fw"></i>
-                <!-- Counter - Messages -->
-                <span class="badge badge-danger badge-counter">7</span>
-              </a>
-              <!-- Dropdown - Messages -->
-              <div class="dropdown-list dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="messagesDropdown">
-                <h6 class="dropdown-header">
-                  Message Center
-                </h6>
-                <a class="dropdown-item d-flex align-items-center" href="#">
-                  <div class="dropdown-list-image mr-3">
-                    <img class="rounded-circle" src="https://source.unsplash.com/fn_BT9fwg_E/60x60" alt="">
-                    <div class="status-indicator bg-success"></div>
-                  </div>
-                  <div class="font-weight-bold">
-                    <div class="text-truncate">Hi there! I am wondering if you can help me with a problem I've been having.</div>
-                    <div class="small text-gray-500">Emily Fowler · 58m</div>
-                  </div>
-                </a>
-                <a class="dropdown-item d-flex align-items-center" href="#">
-                  <div class="dropdown-list-image mr-3">
-                    <img class="rounded-circle" src="https://source.unsplash.com/AU4VPcFN4LE/60x60" alt="">
-                    <div class="status-indicator"></div>
-                  </div>
-                  <div>
-                    <div class="text-truncate">I have the photos that you ordered last month, how would you like them sent to you?</div>
-                    <div class="small text-gray-500">Jae Chun · 1d</div>
-                  </div>
-                </a>
-                <a class="dropdown-item d-flex align-items-center" href="#">
-                  <div class="dropdown-list-image mr-3">
-                    <img class="rounded-circle" src="https://source.unsplash.com/CS2uCrpNzJY/60x60" alt="">
-                    <div class="status-indicator bg-warning"></div>
-                  </div>
-                  <div>
-                    <div class="text-truncate">Last month's report looks great, I am very happy with the progress so far, keep up the good work!</div>
-                    <div class="small text-gray-500">Morgan Alvarez · 2d</div>
-                  </div>
-                </a>
-                <a class="dropdown-item d-flex align-items-center" href="#">
-                  <div class="dropdown-list-image mr-3">
-                    <img class="rounded-circle" src="https://source.unsplash.com/Mv9hjnEUHR4/60x60" alt="">
-                    <div class="status-indicator bg-success"></div>
-                  </div>
-                  <div>
-                    <div class="text-truncate">Am I a good boy? The reason I ask is because someone told me that people say this to all dogs, even if they aren't good...</div>
-                    <div class="small text-gray-500">Chicken the Dog · 2w</div>
-                  </div>
-                </a>
-                <a class="dropdown-item text-center small text-gray-500" href="#">Read More Messages</a>
-              </div>
-            </li>
-
-            <div class="topbar-divider d-none d-sm-block"></div>
-
-            <!-- Nav Item - User Information -->
-            <li class="nav-item dropdown no-arrow">
-              <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <span class="mr-2 d-none d-lg-inline text-gray-600 small">Paciente: <?php echo $Sensores[0]->Paciente; ?></span>
-              </a>
               <!-- Dropdown - User Information -->
               <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
                 <a class="dropdown-item" href="#">
@@ -373,8 +220,52 @@ interval = setInterval(leitura, 5000);
           <!-- Page Heading -->
           <div class="d-sm-flex align-items-center justify-content-between mb-4">
             <h1 class="h3 mb-0 text-gray-800">Visão geral</h1>
-            <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i class="fas fa-download fa-sm text-white-50"></i> Gerar relatório</a>
           </div>
+          <div class="col-sm">
+                 <button type="button" style="font-size:20px ;"class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">
+                   <i class="fas fa-plus"></i> Sensor
+                   </button>
+
+                   <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                     <div class="modal-dialog modal-dialog-centered" role="document">
+                       <div class="modal-content">
+                         <div class="modal-header">
+                           <h5 class="modal-title" id="exampleModalLongTitle">Adicionar Sensor</h5>
+                           <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                             <span aria-hidden="true">&times;</span>
+                           </button>
+                         </div>
+                         <div class="modal-body">
+                         </div>
+
+
+                           <div class="card-body px-5 pt-0 pb-3 my-0">
+                             <form>
+                   <fieldset>
+                     <div class="form-group">
+                       <label>Nome do Cômodo</label>
+                       <input type="text" class="form-control" placeholder="Ex: Quarto Lucas">
+                     </div>
+                     <div class="form-group">
+                       <label>Tipo do Sensor</label>
+                       <select name="sensor" class="form-control">
+                     <option selected disabled>Selecione uma opção</option>
+                     <option value="1">Temperatura/Umidade</option>
+                     <option value="3">Distância</option>
+                     <option value="4">Presença</option>
+
+                       </select>
+                     </div>
+                           <center><button type="button" class="btn btn-secondary" data-dismiss="modalr">Cancelar</button>
+                           <button type="button" class="btn btn-primary">Cadastrar</button></center>
+                         </fieldset>
+                         </form>
+                         </div>
+                         </div>
+                       </div>
+                       </div>
+
+                         </div>
 
           <!-- Content Row -->
           <div class="row">
@@ -400,22 +291,22 @@ foreach($Ambientes as $infoAmbientes){
         <div class="card-body">
         <h4 class="card-title">'.$infoAmbientes->Ambiente.'</h4>';
             foreach($Sensores as $infoSensores){
-                
+
                 if($infoSensores->idAmbiente == $infoAmbientes->idAmbiente){
                     echo '
                     <div class="card m-1 p-1">
                     <div class="card-body">';
 
                     if($infoSensores->Tipo == 1){
-                        
-                        
+
+
                         echo '
-                        
+
                             <div><h5>'.$infoSensores->Nome.'</h5></div>
                             Temperatura
                             <div class="progress">
-                                <div id = "LeituraTemperatura'.$leitura_tem_hum.'" class="progress-bar '; 
-                                
+                                <div id = "LeituraTemperatura'.$leitura_tem_hum.'" class="progress-bar ';
+
                                 if($infoSensores->Status == 1) //Anima apenas barras de sensores ativos
                                  echo 'progress-bar-striped progress-bar-animated bg-success';
                                  else echo 'bg-danger';
@@ -427,8 +318,8 @@ foreach($Ambientes as $infoAmbientes){
                             </div>
                             Humidade
                             <div class="progress">
-                                <div id = "LeituraHumidade'.$leitura_tem_hum.'" class="progress-bar '; 
-                                
+                                <div id = "LeituraHumidade'.$leitura_tem_hum.'" class="progress-bar ';
+
                                 if($infoSensores->Status == 1) //Anima apenas barras de sensores ativos
                                  echo 'progress-bar-striped progress-bar-animated bg-success';
                                  else echo 'bg-danger';
@@ -442,18 +333,18 @@ foreach($Ambientes as $infoAmbientes){
 
                         $leitura_tem_hum++;
 
-                        
+
                     } else if($infoSensores->Tipo == 4){
-                        
-                        
+
+
                         echo '<div><h5>'.$infoSensores->Nome.'</h5></div>';
                         if($infoSensores->LeituraPresenca == 1)
                             echo '<b>Paciente presente</b>';
                         else
                             echo '<b>Paciente ausente</b>';
-                            
+
                     }
-                    echo '                   
+                    echo '
                     <div>Status do Sensor: ';
                     if($infoSensores->Status)
                         echo
@@ -463,11 +354,11 @@ foreach($Ambientes as $infoAmbientes){
                             '<font style="color: #DC143C">Desligado</font>';
                     echo '
                     </div>
-                    <div>Última Leitura: 
+                    <div>Última Leitura:
                     <span id="AtivadoEm'.$leitura_ultima_leitura.'"</span></div>
-                    <div><font style="font-size: 9px;">Ativado em: 
+                    <div><font style="font-size: 9px;">Ativado em:
                     <span id="DesativadoEm'.$leitura_ativado_em.'"></span></font></div>
-                    <div><font style="font-size: 9px;">Desativado em: 
+                    <div><font style="font-size: 9px;">Desativado em:
                     <span id="LeituraEm'.$leitura_desativado_em.'"></span></font></div>
                     </div>
                     </div>
@@ -562,18 +453,18 @@ foreach($Ambientes as $infoAmbientes){
             <!-- Content Column -->
             <div class="col-lg-6 mb-4">
 
-              
-            
-            
+
+
+
 
             </div>
 
             <div class="col-lg-6 mb-4">
 
-              
 
-            
-            
+
+
+
 
             </div>
           </div>
